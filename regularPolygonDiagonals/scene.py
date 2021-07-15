751c53af1c187bc014f71410dfa6e9f627a18af1
dnzc <danchenzq@gmail.com>
2021-07-15 22:37:06 +0100

    stuff

--- regularPolygonDiagonals/scene.py
+++ regularPolygonDiagonals/scene.py
@@ -1,8 +1,16 @@
 from manim import *
 
 
-class SquareToCircle(Scene):
+class Title(Scene):
     def construct(self):
-        circle = Circle()  # create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
-        self.play(Create(circle))  # show the circle on screen
+        text = Text("A general case of toggling diagonals", color=BLUE)
+        self.play(FadeIn(text))
+        self.wait(1.5)
+        self.play(FadeOut(text))
+
+
+class Experimentation(Scene):
+    def construct(self):
+        t1 = Text("This video is about a generalized proof I came up with for Tournament of Towns Nov 1983 question 3.").scale(0.5)
+        t1.align_on_border(UP)
+        self.play(Write(t1))
